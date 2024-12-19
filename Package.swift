cee1f8456423bb77b29e54b0a711c6780db52716
Emilio Peláez <me@epelaez.net>
2024-12-18 21:06:08 -0600

    Update Swift version and Supported Platforms

--- Package.swift
+++ Package.swift
@@ -1,11 +1,11 @@
-// swift-tools-version: 5.6
+// swift-tools-version: 5.10
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
 
 let package = Package(
 	name: "Monarch",
-	platforms: [.iOS(.v13), .watchOS(.v6), .macCatalyst(.v14), .macOS(.v12)],
+	platforms: [.iOS(.v14), .macCatalyst(.v14), .tvOS(.v14), .watchOS(.v7), .macOS(.v11), .visionOS(.v1)],
 	products: [
 		.library(
 			name: "Monarch",
