4edbeb4413f709e6d5d2512c9100ece3d02d02bd
Emilio Peláez <me@epelaez.net>
2024-12-18 21:13:05 -0600

    Platform Updates and Watch Example

---
+++ MonarchExample/MonarchExampleWatch/MonarchExampleWatchApp.swift
@@ -0,0 +1,18 @@
+//
+//  MonarchExampleWatchApp.swift
+//  MonarchExampleWatch Watch App
+//
+//  Created by Emilio Peláez on 12/18/24.
+//
+
+import SwiftUI
+import Monarch
+
+@main
+struct MonarchExampleWatchApp: App {
+	var body: some Scene {
+		WindowGroup {
+			ContentView()
+		}
+	}
+}
